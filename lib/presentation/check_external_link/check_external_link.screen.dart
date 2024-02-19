1d8fc77785fd6dab9d4354e03b16714ab51cd051
slash code <slashcode.dev@gmail.com>
2024-02-19 09:53:51 +0700

    fix

--- lib/presentation/check_external_link/check_external_link.screen.dart
+++ lib/presentation/check_external_link/check_external_link.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import '../../infrastructure/theme/app_colors.dart';
 import '../../utils/assets/assets_images.dart';
 import 'controllers/check_external_link.controller.dart';
 
@@ -8,10 +9,13 @@ class CheckExternalLinkScreen extends GetView<CheckExternalLinkController> {
   const CheckExternalLinkScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: controller.checkingAuth(),
-        builder: (context, snap) {
-          return Center(child: Image.asset(logo));
-        });
+    return Scaffold(
+      backgroundColor: kWhiteBackground,
+      body: FutureBuilder(
+          future: controller.checkingAuth(),
+          builder: (context, snap) {
+            return Center(child: Image.asset(logo));
+          }),
+    );
   }
 }
