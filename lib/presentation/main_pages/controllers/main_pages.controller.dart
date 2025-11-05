0bd2c5bc3d654674bb5c4f1d91ef47a598a431f1
Tedi Lesmana <tedi.lesmana@upercut.fr>
2025-11-05 17:58:17 +0700

    fix

--- lib/presentation/main_pages/controllers/main_pages.controller.dart
+++ lib/presentation/main_pages/controllers/main_pages.controller.dart
@@ -20,7 +20,7 @@ class MainPagesController extends GetxController {
   late StreamSubscription sub;
   var selectedIndex = 0;
   bool isEmployees = false;
-  String versionApp = '2.0.2';
+  String versionApp = '2.0.3';
 
   void changeTabIndex(int index) {
     selectedIndex = index;
